f1bd678911151fd0e3af8080cadf58f49f17a9e7
srishti9612 <srishtishar@gmail.com>
2022-11-01 18:36:55 +0530

    visibility dropdown component

--- components/postModal/index.tsx
+++ components/postModal/index.tsx
@@ -1,84 +1,84 @@
-import { useState } from 'react';
-import React from 'react';
-import Modal from 'react-modal';
-import styles from './index.module.css';
-import Close from '@public/icons/close.svg';
-import VisibilityDropdown from '../visibilityDropdown';
-import Image from 'next/image';
+import { useState } from "react";
+import React from "react";
+import Modal from "react-modal";
+import styles from "./index.module.css";
+import Close from "@public/icons/close.svg";
+import VisibilityDropdown from "../visibilityDropdown";
+import Image from "next/image";
 
 const customStyles = {
-    content: {
-        top: '35%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-        minHeight: '50%',
-        maxHeight: 'max-content',
-        minWidth: '40%',
-        maxWidth: 'max-content',
-        padding: '0px',
-    },
+  content: {
+    top: "35%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    height: "50%",
+    width: "40%",
+    padding: "0px",
+  },
 
-    overlay: {
-        backgroundColor: 'hsl(0, 0%, 90%)',
-    }
+  overlay: {
+    backgroundColor: "hsl(0, 0%, 90%)",
+  },
 };
 
 const PostModal = () => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const profileImgUrl = "https://picsum.photos/200";
-    const userName = "Jane Doe"; 
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const profileImgUrl = "https://picsum.photos/200";
+  const userName = "Jane Doe";
 
-    const openModal = () => {
-        setIsOpen(true);
-    }
+  const openModal = () => {
+    setIsOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsOpen(false);
-    }
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <div>
-            <button onClick={openModal}>Open Modal</button>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-            >
-                <div className={styles.titleWrapper}>
-                    <div className={styles.titleText}>Create a new post</div>
-                    <Close className={styles.closeIcon} onClick={closeModal} />
-                </div>
-                <hr className={styles.line} />
-                <div className={styles.postDetailsWrapper}>
-                    <div className={styles.profileImgWrapper}>
-                        <Image 
-                            width={55} 
-                            height={55}
-                            objectFit="cover"
-                            src={profileImgUrl} 
-                            alt="Profile Picture" />
-                    </div>
-                    <div className={styles.userName}>{userName}</div>
-                    <div className={styles.dropDownWrapper}>
-                        <VisibilityDropdown />
-                    </div>
-                </div>
-                <div>Text box with character limit</div>
-                <hr className={styles.line} />
-                <div>
-                    <div>
-                        <div>Image Icon</div>
-                        <div>Emoticon Icon</div>
-                    </div>
-                    <button>Send Post</button>
-                </div>
-            </Modal>
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div className={styles.titleWrapper}>
+          <div className={styles.titleText}>Create a new post</div>
+          <Close className={styles.closeIcon} onClick={closeModal} />
         </div>
-    )
-}
+        <hr className={styles.line} />
+        <div className={styles.postDetailsWrapper}>
+          <div className={styles.profileImgWrapper}>
+            <Image
+              width={55}
+              height={55}
+              objectFit="cover"
+              src={profileImgUrl}
+              alt="Profile Picture"
+            />
+          </div>
+          <div className={styles.userName}>{userName}</div>
+          <div className={styles.dropDownWrapper}>
+            <VisibilityDropdown />
+          </div>
+        </div>
+        <div>Text box with character limit</div>
+        <hr className={styles.line} />
+        <div>
+          <div>
+            <div>Image Icon</div>
+            <div>Emoticon Icon</div>
+          </div>
+          <button>Send Post</button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
 export default PostModal;
--- components/visibilityDropdown/index.tsx
+++ components/visibilityDropdown/index.tsx
@@ -1,22 +1,70 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import Public from "@public/icons/public.svg";
+import Private from "@public/icons/private.svg";
+import styles from "./index.module.css";
+import Selected from '@public/icons/selected.svg';
 
-type visibilityStates = 'public' | 'private';
+type visibilityStates = "public" | "private";
+
+type stateObj = {
+  name: visibilityStates;
+  details: string;
+  icon: () => any;
+}
 
 const VisibilityDropdown = () => {
-    const [selectedState, setSelectedState] = useState<visibilityStates>('public');
-    const [dropDownVisible, setDropDownVisible] = useState<boolean>(false);
+  const [selectedState, setSelectedState] =
+    useState<visibilityStates>("public");
+  const [dropDownVisible, setDropDownVisible] = useState<boolean>(false);
 
-    const toggleDropDown = () => {
-        setDropDownVisible(!dropDownVisible);
-    }
+  const toggleDropDown = () => {
+    setDropDownVisible(!dropDownVisible);
+  };
 
-    return (
-        <>
-            <div onClick={toggleDropDown}>Visibility DropDown</div>
-            <div style={{ display: dropDownVisible ? 'block' : 'none' }}>Dropdown Menu</div>
-        </>
-    )
-}
+  const visibilityStates: stateObj[] = [
+    {
+      name: "public",
+      details: "Visible to everyone",
+      icon: () => <Public />,
+    },
+    {
+      name: "private",
+      details: "Visible to your followers",
+      icon: () => <Private />,
+    },
+  ];
+
+  const handleSelectState = (visibilitySetting: visibilityStates) => {
+        setSelectedState(visibilitySetting);
+  }
+
+  return (
+    <div className={styles.componentWrapper}>
+      <div className={styles.dropDownHeader} onClick={toggleDropDown}>
+        Visibility DropDown
+      </div>
+      <div
+        className={styles.dropDownMenu}
+        style={{ display: dropDownVisible ? "block" : "none" }}
+      >
+        {visibilityStates.map((item) => {
+          return (
+            <div className={styles.listItem} onClick={() => handleSelectState(item.name)}>
+              <div className={styles.stateWrapper}>
+                <div className={styles.stateDetails}>
+                  <div>{item.icon()}</div>
+                  <div className={styles.stateName}>{item.name}</div>
+                </div>
+                <div className={styles.stateInfo}>{item.details}</div>
+              </div>
+              {(selectedState === item.name) &&<div className={styles.stateSelected}><Selected /></div>}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default VisibilityDropdown;
