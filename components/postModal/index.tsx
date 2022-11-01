4f4685b7563dcabc38ffaa4589957a3512ed3304
srishti9612 <srishtishar@gmail.com>
2022-11-02 00:22:08 +0530

    [add] text box with character limit

--- components/postModal/index.tsx
+++ components/postModal/index.tsx
@@ -20,7 +20,7 @@ const customStyles = {
     boxShadow:
       "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     // minHeight: "50%",
-    height:"max-content", 
+    height: "max-content",
     width: "40%",
     padding: "0px",
     overflow: "hidden",
@@ -33,6 +33,8 @@ const customStyles = {
 
 const PostModal = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [numberUsed, setNumberUsed] = useState(0);
+  const [postText, setPostText] = useState("");
   const profileImgUrl = "https://picsum.photos/200";
   const userName = "Jane Doe";
 
@@ -43,6 +45,11 @@ const PostModal = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
+
+  const handlePostText = (e: any) => {
+    setPostText(e.target.value);
+    setNumberUsed(e.target.value.length);
+  }
 
   return (
     <div>
@@ -72,16 +79,29 @@ const PostModal = () => {
             <VisibilityDropdown />
           </div>
         </div>
-        <div><textarea className={styles.postTextArea} placeholder="What's happening?"></textarea></div>
+        <textarea
+          className={styles.postTextArea}
+          placeholder="What's happening?"
+          maxLength={250}
+          value={postText}
+          onChange={(e) => handlePostText(e)}
+        ></textarea>
+        <div className={styles.charLimitInfoWrapper}>
+          <div className={styles.charLimitInfo}>{numberUsed}/250</div>
+        </div>
         <hr className={styles.actionBarBorder} />
         <div className={styles.actionBar}>
           <div className={styles.actionItems}>
-            <div className={styles.imgUploadWrapper}><UploadImage className={styles.imgUploadIcon}/></div>
-            <div className={styles.emoticonWrapper}><Emoticons className={styles.emoticonIcon}/></div>
+            <div className={styles.imgUploadWrapper}>
+              <UploadImage className={styles.imgUploadIcon} />
+            </div>
+            <div className={styles.emoticonWrapper}>
+              <Emoticons className={styles.emoticonIcon} />
+            </div>
           </div>
           <div className={styles.sendButton}>
             Send Post
-            <Send className={styles.sendIcon}/>
+            <Send className={styles.sendIcon} />
           </div>
         </div>
       </Modal>
